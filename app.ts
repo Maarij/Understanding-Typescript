a262bda018f48cd7837ed5a1977e437f29fcd262
Maarij <maarij.anwar@gmail.com>
2023-05-14 13:09:40 -0500

    enums

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
+enum ROLE { ADMIN, READ_ONLY}
 const person = {
   name: 'Maarij',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: ROLE.ADMIN
 };
 
 console.log(person);
