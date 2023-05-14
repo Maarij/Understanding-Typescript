a2d9a33501d60530ee93fe6e8d90e55bc27343b4
Maarij <maarij.anwar@gmail.com>
2023-05-14 13:17:46 -0500

    union types

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-enum ROLE { ADMIN, READ_ONLY}
-const person = {
-  name: 'Maarij',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: ROLE.ADMIN
-};
+function combine(i1: number | string, i2: number | string) {
+    let result;
+    if (typeof i1 === 'number' && typeof i2 === 'number') {
+        result = i1 + i2;
+    } else {
+        result = i1.toString() + i2.toString();
+    }
 
-console.log(person);
+    return result;
+}
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
