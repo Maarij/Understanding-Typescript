9c01b2c104c328fa6200d4bd05ff0a59671115af
Maarij <maarij.anwar@gmail.com>
2023-05-14 12:54:20 -0500

    object types

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-}
+const person: {
+  name: string,
+  age: number
+} = {
+  name: 'Maarij',
+  age: 30
+};
 
-const n1 = 5;
-const n2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(n1, n2, printResult, resultPhrase);
+console.log(person);
